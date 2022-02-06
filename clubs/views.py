817a42b124dcc8fa54f85ae54625cbde44bda88d
Mugur-Cristian Iacob <mugur-cristian.iacob@kcl.ac.uk>
2022-02-06 00:47:36 +0000

    Implement welcome page and sign up page

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import SignUpForm
+from django.conf import settings
 
-# Create your views here.
+# class LoginProhibitedMixin:
+#
+#      """Mixin that redirects when a user is logged in."""
+#
+#      redirect_when_logged_in_url = None
+#
+#      def dispatch(self, *args, **kwargs):
+#         """Redirect when logged in, or dispatch as normal otherwise."""
+#         if self.request.user.is_authenticated:
+#             return self.handle_already_logged_in(*args, **kwargs)
+#         return super().dispatch(*args, **kwargs)
+#
+#      def handle_already_logged_in(self, *args, **kwargs):
+#          url = self.get_redirect_when_logged_in_url()
+#          return redirect(url)
+#
+#      def get_redirect_when_logged_in_url(self):
+#          """Returns the url to redirect to when not logged in."""
+#          if self.redirect_when_logged_in_url is None:
+#             raise ImproperlyConfigured(
+#              "LoginProhibitedMixin requires either a value for "
+#              "'redirect_when_logged_in_url', or an implementation for "
+#              "'get_redirect_when_logged_in_url()'."
+#              )
+#          else:
+#              return self.redirect_when_logged_in_url
+
+def home(request):
+    current_user = request.user
+    return render(request, 'home.html')
+
+"""This function standardize the requirements for
+    user registration, if the user successfully
+    registers, it will be created in the system,
+    and will be redirected to the profile page """
+class SignUpView(FormView):
+    """View that signs up user."""
+
+    form_class = SignUpForm
+    template_name = "sign_up.html"
+    #redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
+
+    def form_valid(self, form):
+        self.object = form.save()
+        login(self.request, self.object)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        pass
+        #return reverse(settings.REDIRECT_URL_WHEN_LOGGED_IN)
