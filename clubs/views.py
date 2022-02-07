f01d32d976324324efb2540c64073268cd9eaec4
lubnataraki <k20031620@kcl.ac.uk>
2022-02-07 11:56:46 +0000

    Add edit profile option without class based view

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.apps import apps
+from .forms import EditProfileForm
+from clubs.models import User
 
-# Create your views here.
+
+@login_required
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = EditProfileForm(instance=current_user, data=request.POST)
+        if form.is_valid():
+            messages.add_message(request, messages.SUCCESS, "Profile updated!")
+            form.save()
+            return redirect('feed')
+    else:
+        form = EditProfileForm(instance=current_user)
+    return render(request, 'edit_profile.html', {'form': form, 'user': current_user})
--- system/urls.py
+++ system/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from clubs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+
 ]
