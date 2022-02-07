50625a23516a2b9cb0558be7c5730972cd328337
lubnataraki <k20031620@kcl.ac.uk>
2022-02-07 21:38:13 +0000

    Add edit profile functionality

--- system/urls.py
+++ system/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('users/', views.search_users, name='search_users'),
     path('sign_up/', views.SignUpView.as_view(), name='sign_up'),
     path('login/', views.LogInView.as_view(), name='login'),
+    path('edit_profile/', views.EditProfileView.as_view(), name='edit_profile'),
     path('create_club/', views.CreateClubView, name ='create_club'),
 
 ]
