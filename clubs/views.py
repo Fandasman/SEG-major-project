71b2c935f9d10f986eae7e48eef7b38ddf34c05f
Guneek Deol <guneek.deol@kcl.ac.uk>
2022-02-14 14:48:35 +0000

    init

--- clubs/views.py
+++ clubs/views.py
@@ -87,32 +87,32 @@ def search_users(request):
         users= User.objects.all()
     return render(request, 'search_users.html', {'users': users})
 
-# class LoginProhibitedMixin:
+class LoginProhibitedMixin:
     
-#          """Mixin that redirects when a user is logged in."""
+         """Mixin that redirects when a user is logged in."""
     
-#          redirect_when_logged_in_url = None
+         redirect_when_logged_in_url = None
     
-#          def dispatch(self, *args, **kwargs):
-#             """Redirect when logged in, or dispatch as normal otherwise."""
-#             if self.request.user.is_authenticated:
-#                 return self.handle_already_logged_in(*args, **kwargs)
-#             return super().dispatch(*args, **kwargs)
+         def dispatch(self, *args, **kwargs):
+            """Redirect when logged in, or dispatch as normal otherwise."""
+            if self.request.user.is_authenticated:
+                return self.handle_already_logged_in(*args, **kwargs)
+            return super().dispatch(*args, **kwargs)
     
-#          def handle_already_logged_in(self, *args, **kwargs):
-#              url = self.get_redirect_when_logged_in_url()
-#              return redirect('feed')
+         def handle_already_logged_in(self, *args, **kwargs):
+             url = self.get_redirect_when_logged_in_url()
+             return redirect('feed')
     
-#          def get_redirect_when_logged_in_url(self):
-#              """Returns the url to redirect to when not logged in."""
-#              if self.redirect_when_logged_in_url is None:
-#                 raise ImproperlyConfigured(
-#                  "LoginProhibitedMixin requires either a value for "
-#                  "'redirect_when_logged_in_url', or an implementation for "
-#                  "'get_redirect_when_logged_in_url()'."
-#                  )
-#              else:
-#                  return self.redirect_when_logged_in_url
+         def get_redirect_when_logged_in_url(self):
+             """Returns the url to redirect to when not logged in."""
+             if self.redirect_when_logged_in_url is None:
+                raise ImproperlyConfigured(
+                 "LoginProhibitedMixin requires either a value for "
+                 "'redirect_when_logged_in_url', or an implementation for "
+                 "'get_redirect_when_logged_in_url()'."
+                 )
+             else:
+                 return self.redirect_when_logged_in_url
 
 class LogInView(View):
     """Log-in handling view"""
