1279d718a565134c842cda869329f4dc2da8ce3d
Mugur-Cristian Iacob <mugur-cristian.iacob@kcl.ac.uk>
2022-03-09 14:20:16 +0000

    Refine user page

--- system/settings.py
+++ system/settings.py
@@ -14,6 +14,7 @@ from django.contrib.messages import constants as message_constants
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
