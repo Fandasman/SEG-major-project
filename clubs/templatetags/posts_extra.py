9a5473193f99746c80b29cf3b0b2aadc560f83b9
Daphnee Brunschwig <daphnee.brunschwig@kcl.ac.uk>
2022-03-30 16:07:15 +0100

    info alert with link attached implementation for owner club list and member club list pages

--- clubs/templatetags/posts_extra.py
+++ clubs/templatetags/posts_extra.py
@@ -11,3 +11,7 @@ def model_type(value):
 @register.filter
 def has_liked_filter(value,user):
     return value.has_liked(user)
+
+@register.filter
+def has_commented_filter(value,user):
+    return value.has_commented(user)
