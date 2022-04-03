4e249f836e5ef79e51e6d8636bce42ac687e6a75
Mugur-Cristian Iacob <mugur-cristian.iacob@kcl.ac.uk>
2022-04-04 02:20:52 +0300

    Test models -Test Comment model -Test MembershipPost model -Test UserPost model -Create default_comments.json -Create default_membership_post.json -Create default_user_post.json

--- clubs/tests/models/test_comment_model.py
+++ clubs/tests/models/test_comment_model.py
@@ -1,36 +1,78 @@
-from django.core.exceptions import ValidationError
 from django.test import TestCase
-from microblogs.models import Post, User
+from django.core.exceptions import ValidationError
+from clubs.models import User, Club, UserPost,Comment
 
-class PostTest(TestCase):
+class CommentTestCase(TestCase):
+    """Unit tests of the club model."""
 
-    fixtures = ['clubs/tests/fixtures/default_user.json']
+    fixtures = ['clubs/tests/fixtures/default_user.json',
+                'clubs/tests/fixtures/default_club.json',
+                'clubs/tests/fixtures/other_users.json',
+                'clubs/tests/fixtures/default_user_post.json',
+                'clubs/tests/fixtures/default_comment.json',
+                ]
 
     def setUp(self):
-        super(TestCase, self).setUp()
-        self.user = User.objects.get(username='@johndoe')
-        self.post = Post(
-            author=self.user,
-            text="The quick brown fox jumps over the lazy dog."
-        )
+        self.user = User.objects.get(username = "johndoe")
+        self.club = Club.objects.get(name = "Booker")
+        self.post = UserPost.objects.get(author = self.user, club = self.club)
+        self.comment =Comment.objects.get()
 
-    def test_valid_message(self):
-        try:
-            self.post.full_clean()
-        except ValidationError:
-            self.fail("Test message should be valid")
+#Test post field
+    def test_post_field_must_not_be_blank(self):
+       self.comment.post.delete()
+       self._assert_comment_is_invalid()
 
-    def test_author_must_not_be_blank(self):
-        self.post.author = None
+    def test_must_contain_a_post(self):
+        self.comment.post = self.post
+        self._assert_comment_is_valid()
+
+#Test body field
+
+    def test_body_field_cannot_have_more_than_100_charachters(self):
+        self.comment.body = "x" * 101
+        self._assert_comment_is_invalid()
+
+    def test_body_can_contain_100_charachters(self):
+        self.comment.body = "x" * 100
+        self._assert_comment_is_valid()
+
+    def test_body_can_contain_less_than_100_charachters(self):
+        self.comment.body = "x" * 99
+        self._assert_comment_is_valid()
+
+    def test_body_cannot_be_blank(self):
+        self.comment.body =''
+        self._assert_comment_is_invalid()
+
+#Test use field
+
+    def test_user_field_cannot_be_blank(self):
+        self.comment.user.delete()
+        self._assert_comment_is_invalid()
+
+    def test_comment_must_contain_a_user(self):
+        self.comment.user = self.user
+        self._assert_comment_is_valid()
+
+#Test club field
+
+    def test_club_field_must_not_be_blank(self):
+        self.comment.club.delete()
+        self._assert_comment_is_invalid()
+
+    def test_comment_must_contain_a_club(self):
+        self.comment.club = self.club
+        self._assert_comment_is_valid()
+
+
+
+    def _assert_comment_is_valid(self):
+       try:
+           self.comment.full_clean()
+       except (ValidationError):
+           self.fail('Test should be valid')
+
+    def _assert_comment_is_invalid(self):
         with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_text_must_not_be_blank(self):
-        self.post.text = ''
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_text_must_not_be_overlong(self):
-        self.post.text = 'x' * 281
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
+            self.comment.full_clean()
--- clubs/tests/models/test_membership_post.py
+++ clubs/tests/models/test_membership_post.py
@@ -1,36 +1,63 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from microblogs.models import Post, User
+from clubs.models import Club, User, MembershipPost
 
-class PostTest(TestCase):
+class MembershipTestCase(TestCase):
 
-    fixtures = ['microblogs/tests/fixtures/default_user.json']
+    fixtures = ['clubs/tests/fixtures/default_user.json',
+                'clubs/tests/fixtures/default_club.json',
+                'clubs/tests/fixtures/default_membership_post.json'
+                ]
 
     def setUp(self):
-        super(TestCase, self).setUp()
-        self.user = User.objects.get(username='@johndoe')
-        self.post = Post(
-            author=self.user,
-            text="The quick brown fox jumps over the lazy dog."
-        )
+        self.user = User.objects.get(username = "johndoe")
+        self.club = Club.objects.get(name = "Booker")
+        self.membership_post = MembershipPost.objects.get(user = self.user, club = self.club)
 
-    def test_valid_message(self):
-        try:
-            self.post.full_clean()
-        except ValidationError:
-            self.fail("Test message should be valid")
 
-    def test_author_must_not_be_blank(self):
-        self.post.author = None
+# Test user field
+
+    def test_user_field_cannot_be_blank(self):
+        self.membership_post.user.delete()
+        self._assert_membership_post_is_invalid()
+
+    def test_user_field_must_must_have_a_user(self):
+        self.membership_post.user = self.user
+        self._assert_membership_post_is_valid()
+
+# Test club field
+
+    def test_club_field_must_not_be_blank(self):
+        self.membership_post.club.delete()
+        self._assert_membership_post_is_invalid()
+
+    def test_club_field_must_must_have_a_user(self):
+        self.membership_post.club= self.club
+        self._assert_membership_post_is_valid()
+
+
+#Test join field
+    def test_default_value_for_join_is_true(self):
+        self.assertTrue(self.membership_post.join)
+
+#Test description function
+
+    def test_output_of_description_functio_if_join_is_true(self):
+        self.assertEqual("has joined this club",self.membership_post.description())
+
+
+    def test_output_of_description_functio_if_join_is_false(self):
+        self.membership_post.join = False
+        self.assertEqual("has left this club",self.membership_post.description())
+
+
+
+    def _assert_membership_post_is_valid(self):
+       try:
+           self.membership_post.full_clean()
+       except (ValidationError):
+           self.fail('Test should be valid')
+
+    def _assert_membership_post_is_invalid(self):
         with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_text_must_not_be_blank(self):
-        self.post.text = ''
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
-
-    def test_text_must_not_be_overlong(self):
-        self.post.text = 'x' * 281
-        with self.assertRaises(ValidationError):
-            self.post.full_clean()
+            self.membership_post.full_clean()
